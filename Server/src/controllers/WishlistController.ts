3136b82f5ae216946f354e4ba912e8ce8a874b56
Aayushman Sharma <075bme004aayushman@gmail.com>
2024-01-08 10:12:05 +0545

    Add error handling for wishlist

--- Server/src/controllers/WishlistController.ts
+++ Server/src/controllers/WishlistController.ts
@@ -1,39 +1,45 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import WishlistService from "../services/WishlistService";
+import HttpStatus from "http-status-codes";
 
 const WishlistController = {
-  getUserWishlist: async (req: Request, res: Response) => {
+  getUserWishlist: async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const userId = +req.params.userId;
+      const userId = req.params.userId;
       const wishlist = await WishlistService.getUserWishlist(userId);
       res.json(wishlist);
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Internal Server Error" });
+      next(error);
     }
   },
 
-  addToWishlist: async (req: Request, res: Response) => {
+  addToWishlist: async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const userId = +req.params.userId;
-      const productId = +req.params.productId;
+      const userId = req.params.userId;
+      const productId = req.params.productId;
       await WishlistService.addToWishlist(userId, productId);
-      res.json({ message: "Product added to wishlist successfully" });
+      res
+        .status(HttpStatus.OK)
+        .json({ message: "Product added to wishlist successfully" });
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Internal Server Error" });
+      next(error);
     }
   },
 
-  removeFromWishlist: async (req: Request, res: Response) => {
+  removeFromWishlist: async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
     try {
-      const userId = +req.params.userId;
-      const productId = +req.params.productId;
+      const userId = req.params.userId;
+      const productId = req.params.productId;
       await WishlistService.removeFromWishlist(userId, productId);
-      res.json({ message: "Product removed from wishlist successfully" });
+      res
+        .status(HttpStatus.OK)
+        .json({ message: "Product removed from wishlist successfully" });
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Internal Server Error" });
+      next(error);
     }
   },
 };
--- Server/src/services/WishlistService.ts
+++ Server/src/services/WishlistService.ts
@@ -1,18 +1,52 @@
 import Wishlist from "../models/Wishlist";
+import NotFoundError from "../error/NotFoundError";
+import ConflictError from "../error/ConflictError";
 
 const WishlistService = {
-  getUserWishlist: async (userId: number) => {
-    return Wishlist.findAll({
+  getUserWishlist: async (userId: string) => {
+    const allWishlistProducts = await Wishlist.findAll({
       where: { userId },
       include: [{ all: true }],
     });
+
+    if (allWishlistProducts.length === 0) {
+      throw new NotFoundError("Products not found");
+    }
+
+    return allWishlistProducts;
   },
 
-  addToWishlist: async (userId: number, productId: number) => {
-    return Wishlist.create({ userId, productId });
+  addToWishlist: async (userId: string, productId: string) => {
+    try {
+      const existingProduct = await Wishlist.findOne({
+        where: { userId, productId },
+      });
+
+      if (existingProduct) {
+        throw new ConflictError("Product already in the Wishlist");
+      }
+
+      const newWishlistItem = await Wishlist.create({ userId, productId });
+
+      return newWishlistItem;
+    } catch (error) {
+      if (error instanceof ConflictError) {
+        throw error;
+      } else {
+        throw new Error("Failed to add product to wishlist");
+      }
+    }
   },
 
-  removeFromWishlist: async (userId: number, productId: number) => {
+  removeFromWishlist: async (userId: string, productId: string) => {
+    const existingProduct = await Wishlist.findOne({
+      where: { userId, productId },
+    });
+
+    if (!existingProduct) {
+      throw new NotFoundError("Product not found");
+    }
+
     return Wishlist.destroy({
       where: { userId, productId },
     });
