3bdecb032b55c22705773dc91aad5cf66dd088b3
Aayushman Sharma <075bme004aayushman@gmail.com>
2024-01-11 16:44:30 +0545

    Add string literal for each product replacing create element method

--- Frontend/src/pages/products/products.ts
+++ Frontend/src/pages/products/products.ts
@@ -1,11 +1,33 @@
 import { IProduct } from "../../interface";
 import { getProducts } from "../../services/ApiServices";
-import { renderProduct } from "../../utils";
+import { addToCart, addToWishlist } from "../../utils";
 
+function renderProduct(product: IProduct) {
+  const productItem = document.createElement("div");
+  productItem.innerHTML = `
+  <h3>${product.title}</h3>
+  <p>${product.description}</p>
+  <p>Price: ${product.price}</p>
+  <button class="add-to-cart-btn">
+    Add to Cart
+  </button>
+  <button class="add-to-wishlist-btn">
+    Add to Wishlist
+  </button>
+`;
+  const addToCartButton = productItem.querySelector(".add-to-cart-btn");
+  const addToWishlistButton = productItem.querySelector(".add-to-wishlist-btn");
+
+  addToCartButton?.addEventListener("click", () => addToCart(product.id));
+  addToWishlistButton?.addEventListener("click", () =>
+    addToWishlist(product.id)
+  );
+  return productItem;
+}
 export async function renderProducts() {
   const container = document.createElement("div");
   const products: IProduct[] = await getProducts();
-  products.map((product) => {
+  products.forEach((product) => {
     container.appendChild(renderProduct(product));
   });
   console.log(container);
