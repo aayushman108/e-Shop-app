d4da89a20e976dd15711999e6494703662cf36a8
Aayushman Sharma <075bme004aayushman@gmail.com>
2024-01-13 05:27:44 +0545

    Add axios interceptor

--- Frontend/src/services/ApiServices.ts
+++ Frontend/src/services/ApiServices.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import http from "./HttpService";
+import { http } from "./HttpService";
 import { ILogin, ISignup } from "../interface";
 
 export async function signup(formData: ISignup) {
--- Frontend/src/services/HttpService.ts
+++ Frontend/src/services/HttpService.ts
@@ -1,9 +1,50 @@
 import axios, { AxiosInstance } from "axios";
 import { API_BASE_URL } from "../config/config";
+import { navigateToPage } from "../router";
 
-const http: AxiosInstance = axios.create({
+export const http: AxiosInstance = axios.create({
   baseURL: API_BASE_URL,
   //withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-export default http;
+// Request interceptor to attach tokens or headers
+http.interceptors.request.use(
+  (config) => {
+    // Attach an access token from local storage (if available)
+    const accessToken = localStorage.getItem("access_token");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response interceptor to handle common responses
+http.interceptors.response.use(
+  (response) => {
+    const { accessToken, refreshToken } = response.data;
+
+    if (accessToken) {
+      localStorage.setItem("access_token", accessToken);
+    }
+
+    if (refreshToken) {
+      localStorage.setItem("refresh_token", refreshToken);
+    }
+
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      console.error("Unauthorized access. Redirecting to login.");
+      navigateToPage("login");
+    }
+    return Promise.reject(error);
+  }
+);
