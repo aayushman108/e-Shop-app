be289d2a44887298891c1012ba1ff5c3095fb70d
Aayushman Sharma <075bme004aayushman@gmail.com>
2024-01-14 23:11:22 +0545

    Add styling to search page

--- Frontend/src/pages/search/search.ts
+++ Frontend/src/pages/search/search.ts
@@ -3,17 +3,26 @@ import { addToCart, addToWishlist } from "../../services/ApiServices";
 
 function renderProduct(product: IProduct) {
   const productItem = document.createElement("div");
+  productItem.className = "search__item";
   productItem.innerHTML = /* html */ `
-  <h3>${product.productName}</h3>
-  <img src= "${product.imageUrl}" alt= "..." />
-  <p>${product.description}</p>
-  <p>Price: ${product.price}</p>
-  <button class="add-to-cart-btn">
-    Add to Cart
-  </button>
-  <button class="add-to-wishlist-btn">
-    Add to Wishlist
-  </button>
+  <div class="search__item-image">
+    <img src= "${product.imageUrl}" alt= "..." />
+  </div>
+  <div class="search__item-description">
+    <p>${product.productName}</p>
+    <p>$${product.price}</p>
+  </div>
+  <div class="search__item-links">
+    <button class="add-to-cart-btn">
+      <i class="bi bi-cart4"></i>
+    </button>
+    <button class="add-to-wishlist-btn">
+      <i class="bi bi-bag-heart"></i>
+    </button>
+    <button class="see-product-detail">
+      <i class="bi bi-eye"></i>
+    </button>
+  </div>
 `;
   const addToCartButton = productItem.querySelector(
     ".add-to-cart-btn"
@@ -46,6 +55,7 @@ function renderProduct(product: IProduct) {
 }
 export async function renderSearches() {
   const container = document.createElement("div");
+  container.className = "search";
 
   const currentUrl = new URL(window.location.href);
   const searchParam = currentUrl.searchParams.get("query");
