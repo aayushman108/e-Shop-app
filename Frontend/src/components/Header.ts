79ad06dac7bcd007da27f6915cd9df9a7215987c
Aayushman Sharma <075bme004aayushman@gmail.com>
2024-01-14 22:43:40 +0545

    Add styling to navbar

--- Frontend/src/components/Header.ts
+++ Frontend/src/components/Header.ts
@@ -5,9 +5,9 @@ export function renderHeader() {
   const header = document.createElement("header");
   header.className = "header";
   header.innerHTML = /* html */ `
-    <nav class="navbar navbar-expand-lg fixed-top bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg fixed-top">
       <div class="container-fluid">
-        <a class="navbar-brand d-lg-none" href="#">e-shop</a>
+        <a class="navbar-brand custom d-lg-none" href="#">e-shop</a>
         <button
           class="navbar-toggler"
           type="button"
@@ -20,34 +20,36 @@ export function renderHeader() {
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <div class="d-flex justify-content-between px-4" style="border: 2px solid black; width: 100%;">
-            <a class="navbar-brand d-none d-lg-block" href="#">e-shop</a>
+          <div class="d-flex justify-content-between px-4" style="width: 100%;">
+            <a class="navbar-brand custom d-none d-lg-block" href="#">e-shop</a>
             <ul class="navbar-nav mb-2 mb-lg-0 order-lg-2 order-sm-1">
               <li class="nav-item">
-                <a class="nav-link" id="home" href="#">Home</a>
+                <a class="nav-link nav-item__link" id="home" href="#">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" id="products" href="#">Products</a>
+                <a class="nav-link nav-item__link" id="products" href="#">Products</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">User</a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item nav-item__link" id="login" href="#">Login</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item nav-item__link" id="signup" href="#">Signup</a></li>
+                </ul>
               </li>
               <li class="nav-item">
-                <a class="nav-link" id="login" href="#">Login</a>
+                <a class="nav-link nav-item__link" id="wishlist" href="#">Wishlist</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" id="signup" href="#">Signup</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" id="wishlist" href="#">Wishlist</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" id="cart" href="#">Cart</a>
+                <a class="nav-link nav-item__link" id="cart" href="#">Cart</a>
               </li>
             </ul>
             <form class="order-lg-1 order-sm-2" role="search">
               <input
-                class="form-control me-2"
+                class="me-2"
                 type="text"
                 id="searchInput"
-                placeholder="Search"
+                placeholder="Search..."
                 aria-label="Search"
               />
             </form>
@@ -58,7 +60,7 @@ export function renderHeader() {
   `;
 
   const links = header.querySelectorAll(
-    ".nav-link"
+    ".nav-item__link"
   ) as NodeListOf<HTMLAnchorElement>;
   links.forEach((link) => {
     link.addEventListener("click", handleNavLinkClick);
